b4d2c17e002e16ea819f555c6b77a7197ad87232
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-12 00:16:50 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -18,8 +18,37 @@ end
 
 
 def apply_coupons(cart, coupons)
-  # code here
-end
+  def apply_coupons(cart, coupons)
+    # code here
+    if coupons.length == 0
+      return cart
+    else
+
+      coupons.each do |index|
+        #index = coupon
+        #item = cart item
+        name = index[:item]
+        item = cart[name]
+
+        if(item != nil)
+          #update curent item
+          if(item[:count] >= index[:num])
+            item[:count] = item[:count] - index[:num]
+            #add coupon item
+            couponkey = "#{name} W/COUPON"
+            if(cart[couponkey] == nil)
+              couponitem = {:price => index[:cost], :clearance => item[:clearance], :count => 1}
+              cart[couponkey] = couponitem
+            else
+              couponitem = cart[couponkey];
+              couponitem[:count] +=1;
+            end
+          end
+        end
+      end
+     end
+    cart
+  endend
 
 def apply_clearance(cart)
   # code here
