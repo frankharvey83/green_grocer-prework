fddfafc0afc9a5ec63343b4084853c808d363e1c
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-12 00:41:50 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -53,9 +53,9 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  cart = consolidate_cart(cart:cart)
-  cart = apply_coupons(cart:cart, coupons:coupons)
-  cart = apply_clearance(cart:cart)
+  cart = consolidate_cart(:cart)
+  cart = apply_coupons(:cart, :coupons)
+  cart = apply_clearance(:cart)
   total = 0
 
   cart.each do |food, info|
